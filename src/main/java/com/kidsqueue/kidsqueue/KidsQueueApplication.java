fabaac6770845c5cb3428e54fceeb05f2e972f1c
k9want <zndwlrnjr26@daum.net>
2024-03-02 20:33:37 +0900

    init project

---
+++ src/main/java/com/kidsqueue/kidsqueue/KidsQueueApplication.java
@@ -0,0 +1,13 @@
+package com.kidsqueue.kidsqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kidsqueue/kidsqueue/KidsQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidsqueue.kidsqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
