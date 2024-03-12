3e484a631ea1d703e99b1719031235d644140711
gyuwon <gyuwoneee@gmail.com>
2024-03-12 16:15:27 +0900

    로그인 필터 구현

--- src/main/java/com/kidsqueue/kidsqueue/config/SecurityConfig.java
+++ src/main/java/com/kidsqueue/kidsqueue/config/SecurityConfig.java
@@ -1,17 +1,36 @@
 package com.kidsqueue.kidsqueue.config;
 
+import com.kidsqueue.kidsqueue.parent.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion을 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     BCryptPasswordEncoder encode() {
@@ -19,21 +38,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,
-        HandlerMappingIntrospector introspector)
-        throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(new MvcRequestMatcher(introspector, "/**"))
-                .permitAll() // 이 주소는 모두에게 허용
-                .anyRequest().authenticated()) // 그 외 나머지 주소는 인증된 사용자에게만 허용
-            .formLogin((formLogin) -> formLogin
-                .loginPage("/auth/login") // 로그인 페이지 주소
-                .defaultSuccessUrl("/index")); // 로그인 시 랜딩 페이지로 리다이렉트
-        http
-            .logout((logout) -> logout.logoutSuccessUrl("/auth/login") // 로그아웃 시 향하는 주소
-                .invalidateHttpSession(true));
-        http.csrf((csrf) -> csrf.disable());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // csrf disable
+        // session 방식에서는 session이 고정되므로 csrf 공격이 항상 방어되어야 함.
+        // 반면 jwt 방식은 세션을 stateless 상태로 관리하므로 csrf 에 대한 공격을 방어하지 않아도 되어서 csrf를 disable
+        http.csrf((auth) -> auth.disable());
+
+        // From 로그인 방식 disable
+        http.formLogin((auth) -> auth.disable());
+
+        // http basic 인증 방식 disable
+        http.httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가 작업
+        http.authorizeHttpRequests(
+            (auth) -> auth.requestMatchers("/**").permitAll().anyRequest().authenticated());
+
+        //LoginFilter 추가 (AuthenticationManager 메소드에 authenticationConfiguration 객체를 넣어야 함)
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)),
+            UsernamePasswordAuthenticationFilter.class);
+
+        // 세션 stateless 상태로 설정
+        http.sessionManagement(
+            (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
