cc5d74566d4c6c8f126a2d969483ee509c0e9b21
song <ssogg5309@naver.com>
2024-03-13 19:59:43 +0900

    Feat: 병원이름으로 검색시 정확도 순으로 병원리스트 가져오기

--- src/main/java/com/kidsqueue/kidsqueue/hospital/controller/HospitalController.java
+++ src/main/java/com/kidsqueue/kidsqueue/hospital/controller/HospitalController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.data.domain.Pageable;
@@ -20,16 +21,44 @@ public class HospitalController {
 
     private final HospitalService hospitalService;
 
+    //  조건에 맞는 병원 리스트 가져오기
     @GetMapping
     public ApiResponse<List<HospitalDto>> get(
-            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
+            @PageableDefault(page = 0, size = 10) Pageable pageable,
+            @RequestParam(required = false) String sortCondition,
+            @RequestParam(required = false) String name
     ) {
 
-        ApiResponse<List<HospitalDto>> apiResponse = hospitalService.findAll(pageable);
+        System.out.println("sortCondition = " + sortCondition);
+        System.out.println("name = " + name);
+
+        ApiResponse<List<HospitalDto>> apiResponse;
+
+        //  등록일 기준 내림차순인 경우
+        if ("desc".equals(sortCondition)) {
+            apiResponse = hospitalService.findAllDesc(pageable);
+            apiResponse.setMessage("등록일 기준 내림차순으로 병원리스트 가져옴");
+        }
+
+        //  병원명으로 검색한 경우
+        else if ("name".equals(sortCondition)) {
+            apiResponse = hospitalService.findAllByName(name, pageable);
+            apiResponse.setMessage("병원명에 검색조건을 포함하는 병원리스트 정확도 순으로 가져옴");
+        }
+
+        //  등록일 기준 오름차순인 경우
+        else if ("asc".equals(sortCondition)) {
+            apiResponse = hospitalService.findAllAsc(pageable);
+            apiResponse.setMessage("등록일 기준 오름차순으로 병원리스트 가져옴");
+        }
+
+        //  아무조건 없는 경우
+        else {
+            apiResponse = hospitalService.findAllAsc(pageable);
+            apiResponse.setMessage("등록일 기준 오름차순으로 병원리스트 가져옴");
+        }
 
         apiResponse.setStatus(ApiResponse.SUCCESS_STATUS);
-        apiResponse.setMessage("응답 성공");
-
         return apiResponse;
     }
 
--- src/main/java/com/kidsqueue/kidsqueue/hospital/service/HospitalService.java
+++ src/main/java/com/kidsqueue/kidsqueue/hospital/service/HospitalService.java
@@ -2,6 +2,7 @@ package com.kidsqueue.kidsqueue.hospital.service;
 
 import com.kidsqueue.kidsqueue.common.ApiResponse;
 import com.kidsqueue.kidsqueue.common.Pagination;
+import com.kidsqueue.kidsqueue.hospital.db.Hospital;
 import com.kidsqueue.kidsqueue.hospital.db.HospitalRepository;
 import com.kidsqueue.kidsqueue.hospital.model.HospitalDto;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,49 @@ public class HospitalService {
     private final HospitalRepository hospitalRepository;
     private final HospitalConverter hospitalConverter;
 
-    public ApiResponse<List<HospitalDto>> findAll(Pageable pageable) {
+    public ApiResponse<List<HospitalDto>> findAllAsc(Pageable pageable) {
 
-        Page<HospitalDto> hospitalDtoPage = hospitalRepository.findAllByIsActive(1, pageable)
+        //  Page<Hospital> 을 Page<HospitalDto> 로 변환
+        Page<HospitalDto> hospitalDtoPage = hospitalRepository.findAllByIsActiveOrderByIdAsc(1, pageable)
                 .map(hospitalConverter::toDto);
 
+        //  Page<HospitalDto> 로 ApiResponse<List<HospitalDto>> 얻어와서 리턴
+        return getApiResponse(hospitalDtoPage);
+
+
+    }
+
+    public ApiResponse<List<HospitalDto>> findAllDesc(Pageable pageable) {
+
+        //  Page<Hospital> 을 Page<HospitalDto> 로 변환
+        Page<HospitalDto> hospitalDtoPage = hospitalRepository.findAllByIsActiveOrderByIdDesc(1, pageable)
+                .map(hospitalConverter::toDto);
+
+        //  Page<HospitalDto> 로 ApiResponse<List<HospitalDto>> 얻어와서 리턴
+        return getApiResponse(hospitalDtoPage);
+
+    }
+
+    public ApiResponse<List<HospitalDto>> findAllByName(String name, Pageable pageable) {
+
+        //  Page<Hospital> 을 Page<HospitalDto> 로 변환
+        Page<HospitalDto> hospitalDtoPage = hospitalRepository.findAllByIsActiveAndNameNative(1, name, pageable)
+                .map(hospitalConverter::toDto);
+
+        //  Page<HospitalDto> 로 ApiResponse<List<HospitalDto>> 얻어와서 리턴
+        return getApiResponse(hospitalDtoPage);
+
+
+
+    }
+
+    //  Page<HospitalDto> 로 ApiResponse<List<HospitalDto>> 만드는 메서드
+    private static ApiResponse<List<HospitalDto>> getApiResponse(Page<HospitalDto> hospitalDtoPage) {
+
+        //  Page<HospitalDto> 에서 List<HospitalDto> 가져오기
         List<HospitalDto> hospitalDtoList = hospitalDtoPage.toList();
 
+        //  Page<HospitalDto> 로 Pagination 생성
         Pagination pagination = Pagination.builder()
                 .page(hospitalDtoPage.getNumber())
                 .size(hospitalDtoPage.getSize())
@@ -33,12 +70,11 @@ public class HospitalService {
                 .totalPage(hospitalDtoPage.getTotalPages())
                 .build();
 
-        ApiResponse<List<HospitalDto>> apiResponse = ApiResponse.<List<HospitalDto>>builder()
+        //  ApiResponse<List<HospitalDto>> 만들어서 리턴
+        return  ApiResponse.<List<HospitalDto>>builder()
                 .data(hospitalDtoList)
                 .pagination(pagination)
                 .build();
-
-        return apiResponse;
-
     }
+
 }
