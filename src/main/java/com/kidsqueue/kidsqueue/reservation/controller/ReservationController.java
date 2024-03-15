33ebc569f6119d52869083ecc567d65f353a9960
sinsj5858 <goddnjswns58@gmail.com>
2024-03-15 22:50:15 +0900

    ReservationController url 변경

--- src/main/java/com/kidsqueue/kidsqueue/reservation/controller/ReservationController.java
+++ src/main/java/com/kidsqueue/kidsqueue/reservation/controller/ReservationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,7 +72,7 @@ public class ReservationController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PatchMapping("/{reservationId}")
+    @PutMapping("/{reservationId}")
     public ResponseEntity<ApiResponse<String>> deleteReservation(
         @PathVariable Long userId,
         @PathVariable Long reservationId) {
